d3702da3ef1740e1f72f45195951f2ee114c18af
Guonejo <ma.carvachos@duocuc.cl>
2024-05-14 13:28:49 -0400

    Api google maps para ubicacion de store

--- js/main.js
+++ js/main.js
@@ -118,3 +118,15 @@ $(document).ready(function () {
           }
       });
   })
+
+
+  var map = L.map('map').setView([-33.01232, -71.54920], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([-33.01232, -71.54920]).addTo(map);
+var popup = L.popup()
+    .setLatLng([-33.01232, -71.54920])
+    .setContent("PurrfectStore")
+    .openOn(map);

7cfaab2821a209c51878939a7b876fc7acd78a97
fl-oneto <fl.oneto@duocuc.cl>
2024-05-14 22:43:26 -0400

    se agrega localstorage

--- js/main.js
+++ js/main.js
@@ -1,132 +1,141 @@
 // jQuery
-$(document).ready(function () {
-    $("#fila-productos").hide();
+$(document).ready(function () { 
+  $("#fila-productos").hide(); //al cargar el documento, esconde una fila de productos 
 
 
-    var savedCount = localStorage.getItem('carritoCount');
-    if (savedCount) {
-        $("#items-carrito").text(savedCount);
-    }
-  
-    // Acción para el botón que muestre la siguiente fila de productos
-    $("#btn-ver-mas").click(function () {
-      $("#fila-productos").show();
-      $("#btn-ver-mas").hide();
-    });
+  var contadorGuardado = localStorage.getItem('carritoCount'); //recupera el valor de contadorGuardado desde el localstorage
+  if (contadorGuardado) { //si contadorGuardado tiene un valor, se actualiza la badge con el conteo de productos en el carrito
+      $("#items-carrito").text(contadorGuardado);
+  }
 
-    
-  
-    $(".btn-add-cart").click(function () {
-      // Obtiene el elemento que contiene el número de elementos en el carrito
-      var itemsCarrito = $("#items-carrito");
-      var nombreProducto = document.querySelector(".nombre-producto").textContent;
-
-      var precioProductoText = document.querySelector(".precio").textContent;
-      
-      var precioProducto = parseFloat(precioProductoText.replace(/[^\d]/g, ''), 10);
-      console.log("Producto comprado:", nombreProducto);
-      console.log("Precio:", precioProducto);
-      console.log("Se agregó un elemento al carrito.");
-  
-      // Incrementa el número de elementos en el carrito
-      var currentQuantity = parseInt($("#items-carrito").text());
-      if (!isNaN(currentQuantity)) {
-        // Si es un número válido, incrementar
-        itemsCarrito.text(currentQuantity + 1);
-      } else {
-        // Si no es un número válido (NaN), establecer el valor inicial a 1
-        itemsCarrito.text(1);
-      }
-      // Guardar el número de elementos en el carrito en localStorage
-      localStorage.setItem('carritoCount', currentQuantity + 1);
-    });
-    
+  // funcion que muestra una fila mas de productos
+  $("#btn-ver-mas").click(function () {
+    $("#fila-productos").show();
+    $("#btn-ver-mas").hide();
   });
+
   
-  function validar() {
-    var correo = document.getElementById("email").value;
-    var nombre = document.getElementById("contact_name").value;
-    var telefono = document.getElementById("contact_phone").value;
-    var mensaje = document.getElementById("contact_message").value;
-  
-    var validacEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    var validacCelular = /^\d{9}$/;
-  
-    if (correo === "" || validacEmail.test(correo) === false) {
-      document.getElementById("mensajeCorreo").textContent =
-        "Debe ingresar un correo válido";
+
+  $(".btn-add-cart").click(function () {
+    // obtiene el número de elementos en el carrito (badge)
+    var itemsCarrito = $("#items-carrito");
+
+    // incrementa el numero de elementos en el carrito (al hacer click en comprar)
+    var cantidad = parseInt($("#items-carrito").text());
+    if (!isNaN(cantidad)) {
+      // si es un número valido, aumenta en 1 
+      itemsCarrito.text(cantidad + 1);
     } else {
-      document.getElementById("mensajeCorreo").textContent = "";
+      // si no es un número valido (NaN), establecer el valor inicial a 1 (cuando el cliente aún no ha agregado nada al carrito)
+      itemsCarrito.text(1);
     }
+    // guardar el número de elementos en el carrito en localStorage para que se mantenga al navegar por la página
+    localStorage.setItem('carritoCount', cantidad + 1);
+  });
+    
+});
   
-    if (telefono === "") {
-      document.getElementById("mensajeTelefono").textContent = "";
-    } else if (validacCelular.test(telefono) === false) {
-      document.getElementById("mensajeTelefono").textContent =
-        "Debe ingresar un teléfono válido";
-    }
-    if (nombre === "") {
-      document.getElementById("mensajeNombre").textContent =
-        "Debe ingresar un nombre ";
-    } else {
-      document.getElementById("mensajeNombre").textContent = "";
-    }
-    if (mensaje === "") {
-      document.getElementById("mensajeMensaje").textContent =
-        "El mensaje no puede estar vacío";
-    } else {
-      document.getElementById("mensajeMensaje").textContent = "";
-    }
-    console.log(mensajeMensaje);
-  }
+function validar() {
+  //se recuperan los datos del formulario
+  var correo = document.getElementById("email").value;
+  var nombre = document.getElementById("contact_name").value;
+  var telefono = document.getElementById("contact_phone").value;
+  var mensaje = document.getElementById("contact_message").value;
   
-  function calcularSubTotal() {
-    var precioProductoText =
-      document.getElementById("precioProducto").textContent;
-    var precioProducto = parseFloat(precioProductoText);
-    var cantidadProducto = parseFloat(
-      document.getElementById("cantidad_producto").value
-    );
-    console.log(precioProductoText);
-    console.log(precioProducto);
-    console.log(cantidadProducto);
+  //se valida que el mail y telefono tengan el formato adecuado
+  var validacEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var validacCelular = /^\d{9}$/;
   
-    document.getElementById("subtotal").textContent =
-      precioProducto * cantidadProducto;
+  //mensajes para permitir al usuario ingresar datos válidos
+  if (correo === "" || validacEmail.test(correo) === false) {
+    document.getElementById("mensajeCorreo").textContent =
+      "Debe ingresar un correo válido";
+  } else {
+    document.getElementById("mensajeCorreo").textContent = "";
   }
 
-  fetch("https://api.thecatapi.com/v1/images/search?limit=3")
-  .then(response => response.json())
-  .then(data => {
-      var container1 = document.getElementById("carrusel-gato1");
-      var container2 = document.getElementById("carrusel-gato2");
-      var container3 = document.getElementById("carrusel-gato3");
+  if (telefono === "") {
+    document.getElementById("mensajeTelefono").textContent = "";
+  } else if (validacCelular.test(telefono) === false) {
+    document.getElementById("mensajeTelefono").textContent =
+      "Debe ingresar un teléfono válido";
+  }
+  if (nombre === "") {
+    document.getElementById("mensajeNombre").textContent =
+      "Debe ingresar un nombre ";
+  } else {
+    document.getElementById("mensajeNombre").textContent = "";
+  }
+  if (mensaje === "") {
+    document.getElementById("mensajeMensaje").textContent =
+      "El mensaje no puede estar vacío";
+  } else {
+    document.getElementById("mensajeMensaje").textContent = "";
+  }
+}
   
-      // itera sobre cada imagen y la agrega al contenedor correspondiente
-      data.forEach((image, index) => {
-          var imageUrl = image.url;
-          var imggato = document.createElement("img");
-          imggato.src = imageUrl;
-  
-          // agrega la imagen al contenedor correspondiente segun el indice
-          if (index === 0) {
-              container1.appendChild(imggato);
-          } else if (index === 1) {
-              container2.appendChild(imggato);
-          } else if (index === 2) {
-              container3.appendChild(imggato);
-          }
-      });
-  })
+function calcularSubTotal() { //funcion que calcula el subtotal del producto 
+  var precioProductoText =
+    document.getElementById("precioProducto").textContent;
+  var precioProducto = parseFloat(precioProductoText);
+  var cantidadProducto = parseFloat(
+    document.getElementById("cantidad_producto").value
+  );
+
+  document.getElementById("subtotal").textContent =
+    precioProducto * cantidadProducto;
+}
+
+//-----consumo de API-------
+// Verificar si los datos están en el localStorage
+var imagenesGuardadas = localStorage.getItem('imagenesGatos');
+
+if (imagenesGuardadas) {
+    // Si hay datos en el localStorage, utilizarlos directamente
+    populateCategories(JSON.parse(imagenesGuardadas));
+} else {
+    fetch("https://api.thecatapi.com/v1/images/search?limit=3") //api que entrega imágenes aleatorias de gatos
+        .then(response => response.json())
+        .then(data => {
+            localStorage.setItem('imagenesGatos', JSON.stringify(data));
+            populateCategories(data);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+// función agregar las imagenes al carrusel
+function populateCategories(data) {
+    var container1 = document.getElementById("carrusel-gato1");
+    var container2 = document.getElementById("carrusel-gato2");
+    var container3 = document.getElementById("carrusel-gato3");
+
+    // itera sobre cada imagen y la agrega al contenedor correspondiente
+    data.forEach((image, index) => {
+        var imageUrl = image.url;
+        var imggato = document.createElement("img");
+        imggato.src = imageUrl;
+
+        // agrega la imagen al contenedor correspondiente segun el indice
+        if (index === 0) {
+            container1.appendChild(imggato);
+        } else if (index === 1) {
+            container2.appendChild(imggato);
+        } else if (index === 2) {
+            container3.appendChild(imggato);
+        }
+    });
+}
 
 
-  var map = L.map('map').setView([-33.01232, -71.54920], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
+
+var map = L.map('map').setView([-33.01232, -71.54920], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([-33.01232, -71.54920]).addTo(map);
 var popup = L.popup()
-    .setLatLng([-33.01232, -71.54920])
-    .setContent("PurrfectStore")
-    .openOn(map);
+  .setLatLng([-33.01232, -71.54920])
+  .setContent("PurrfectStore")
+  .openOn(map);
